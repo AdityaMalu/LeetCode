055483960b96c7e24b336ee082ac30a2860576d4
Aditya Malu <98596211+AdityaMalu@users.noreply.github.com>
2024-07-12 10:19:17 +0530

    Time: 2 ms (61.93%), Space: 9.2 MB (51.83%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void rec(vector<int> & nums, int ind, vector<vector<int>>&ans){
+        if(ind == nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i = ind ; i < nums.size(); i++){
+            swap(nums[ind],nums[i]);
+            rec(nums,ind+1,ans);
+            swap(nums[ind],nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        rec(nums,0,ans);
+        return ans;
+    }
+};
