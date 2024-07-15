9971a5410c2f48280436d00df1f9e9ad835ebdfe
Aditya Malu <98596211+AdityaMalu@users.noreply.github.com>
2024-07-15 15:49:19 +0530

    Time: 14 ms (95.11%), Space: 8.1 MB (89.7%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-int lcsUtil(string& s1, string& s2, int ind1, int ind2, vector<vector<int>>& dp) {
-    if (ind1 < 0 || ind2 < 0)
-        return 0;
+    int longestCommonSubsequence(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
 
-    if (dp[ind1][ind2] != -1)
-        return dp[ind1][ind2];
+        vector<int> prev(m + 1, 0), cur(m + 1, 0);
 
-    if (s1[ind1] == s2[ind2])
-        return dp[ind1][ind2] = 1 + lcsUtil(s1, s2, ind1 - 1, ind2 - 1, dp);
-    else
-        return dp[ind1][ind2] = max(lcsUtil(s1, s2, ind1, ind2 - 1, dp), lcsUtil(s1, s2, ind1 - 1, ind2, dp));
-}
 
-    int longestCommonSubsequence(string text1, string text2) {
-        
-        vector<vector<int>> dp(text1.size(),vector<int>(text2.size(),-1));
-        return lcsUtil(text1,text2,text1.size()-1,text2.size()-1,dp);
+        for (int ind1 = 1; ind1 <= n; ind1++) {
+            for (int ind2 = 1; ind2 <= m; ind2++) {
+                if (s1[ind1 - 1] == s2[ind2 - 1])
+                    cur[ind2] = 1 + prev[ind2 - 1]; 
+                else
+                    cur[ind2] = max(prev[ind2], cur[ind2 - 1]); 
+            }
+            prev = cur;
+        }
+
+        return prev[m];
     }
 };
