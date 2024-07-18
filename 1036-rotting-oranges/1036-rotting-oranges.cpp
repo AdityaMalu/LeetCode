613db220862fb9b32c385144c65a9c272be4a2f5
Aditya Malu <98596211+AdityaMalu@users.noreply.github.com>
2024-07-18 19:08:45 +0530

    Time: 3 ms (84.14%), Space: 16.5 MB (15.78%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,49 +1,50 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int r = grid.size();
-        int c = grid[0].size();
-        int cntFresh = 0;
-        vector<vector<int>> vis(r, vector<int>(c, 0));
-        queue<pair<pair<int, int>, int>> q;
+        int rows = grid.size();
+        int cols = grid[0].size();
+        vector<vector<int>> vis = grid;
+        int ans = 0;
+        queue<pair<pair<int,int>,int>> q;
 
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (grid[i][j] == 2) {
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(grid[i][j] == 2){
                     q.push({{i, j}, 0});
-                    vis[i][j] = 2;
-                }
-                if (grid[i][j] == 1) {
-                    cntFresh++;
                 }
             }
         }
 
-        int time = 0;
-        int drow[] = {-1, 0, 1, 0};
-        int dcol[] = {0, 1, 0, -1};
-        int cnt = 0;
+        int rtrav[] = {-1, 0, 1, 0};
+        int ctrav[] = {0, 1, 0, -1};
 
-        while (!q.empty()) {
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int t = q.front().second;
-            time = max(time, t);
+        while(!q.empty()){
+            auto curr = q.front();
             q.pop();
+            int row = curr.first.first;
+            int col = curr.first.second;
+            int time = curr.second;
+            ans = max(ans, time);
+            vis[row][col] = 2;
 
-            for (int i = 0; i < 4; i++) {
-                int nrow = row + drow[i];
-                int ncol = col + dcol[i];
+            for(int i = 0; i < 4; i++){
+                int newRow = row + rtrav[i];
+                int newCol = col + ctrav[i];
 
-                if (nrow >= 0 && nrow < r && ncol >= 0 && ncol < c && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1) {
-                    q.push({{nrow, ncol}, t + 1});
-                    vis[nrow][ncol] = 2;
-                    cnt++;
+                if(newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && vis[newRow][newCol] == 1){
+                    q.push({{newRow, newCol}, time + 1});
+                    vis[newRow][newCol] = 2;
+                }
+            }
+        }
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(vis[i][j] == 1){
+                    return -1; 
                 }
             }
         }
 
-        if (cntFresh != cnt) return -1;
-        return time;
+        return ans;
     }
 };
