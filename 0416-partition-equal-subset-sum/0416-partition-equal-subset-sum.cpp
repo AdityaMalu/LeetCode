d97cd4a5bb54c278dbc4f29fac1f319998cce403
Aditya Malu <98596211+AdityaMalu@users.noreply.github.com>
2024-07-21 12:44:39 +0530

    Time: 290 ms (25.15%), Space: 16.5 MB (60.52%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,31 +1,59 @@
 class Solution {
-private:
-    bool c(vector<int>& arr, int i, int sum, vector<vector<int>>& dp) {
-        if (sum == 0) {
-            return true;
-        }
-        if (i == arr.size() - 1) {
-            if (sum == 0 || sum - arr[i] == 0) {
-                return true;
-            }
+public:
+
+    bool recc(vector<int> &nums, int ind, int sum,int target,vector<vector<int>> &dp){
+        
+        if(sum >= target || ind >= nums.size()){
+            if(sum == target) return true;
             return false;
         }
-        if (sum < 0) return false;
-        if (dp[i][sum] != -1) {
-            return dp[i][sum] == 1;
-        }
 
-        bool b = c(arr, i + 1, sum - arr[i], dp) || c(arr, i + 1, sum, dp);
-        dp[i][sum] = b ? 1 : 0;
-        return b;
+        if(dp[ind][sum] != -1){
+            return dp[ind][sum] == 1;
+        }
+        bool not_pick = recc(nums,ind+1,sum,target,dp);
+        bool pick = false;
+        if(sum < target){
+            pick = recc(nums,ind+1,sum+nums[ind],target,dp);
+        }
+        if(pick||not_pick) dp[ind][sum] = 1;
+        else dp[ind][sum] = 0;
+        return pick || not_pick;
     }
-public:
+
     bool canPartition(vector<int>& nums) {
-        int totsum = 0;
-        for (int num : nums) totsum += num;
-        if (totsum % 2 == 1) return false;
+        // int sum1 = accumulate(nums.begin() , nums.end() , 0);
+        // vector<vector<int>> dp(nums.size(),vector<int>(sum1/2+1,-1));
+        // if(sum1%2 == 1){
+        //     return false;
+        // }
+        // else{
+        //     return recc(nums,0,0,sum1/2,dp);
+        // }
+
+        int sum1 = accumulate(nums.begin(), nums.end(), 0);
+        if (sum1 % 2 != 0) {
+            return false;
+        }
+        int target = sum1 / 2;
+        int n = nums.size();
         
-        vector<vector<int>> dp(nums.size(), vector<int>(totsum / 2 + 1, -1));
-        return c(nums, 0, totsum / 2, dp);
+        vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
+        
+        for (int i = 0; i <= n; ++i) {
+            dp[i][0] = true;
+        }
+        
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= target; ++j) {
+                if (j >= nums[i - 1]) {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        
+        return dp[n][target];
     }
 };
