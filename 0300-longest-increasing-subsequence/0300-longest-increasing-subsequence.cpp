afff7b48068c5b0d003e8408aa6c58775fbf411f
Aditya Malu <98596211+AdityaMalu@users.noreply.github.com>
2024-07-27 22:15:13 +0530

    Time: 504 ms (20.77%), Space: 295 MB (8.2%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,27 +1,29 @@
-class Solution {
+class Solution
+{
 public:
-    int recurr(int ind,int prev_ind,vector<vector<int>> &dp, vector<int> &nums){
-        if(ind == nums.size()){
+    int recc(vector<int> &nums, int ind, int prev, vector<vector<int>> &dp)
+    {
+        if (ind == nums.size())
+        {
             return 0;
         }
 
-        if(dp[ind][prev_ind + 1] != -1){
-            return dp[ind][prev_ind+1];
+        if (dp[ind][prev + 1] != INT_MIN)
+            return dp[ind][prev + 1];
+
+        int pick = 0, npick;
+        npick = recc(nums, ind + 1, prev, dp);
+        if (prev == -1 || nums[ind] > nums[prev])
+        {
+            pick = 1 + recc(nums, ind + 1, ind, dp);
         }
 
-        int take = 0;
-
-        int not_take = recurr(ind+1,prev_ind,dp,nums);
-
-        if(prev_ind == -1 || nums[ind] > nums[prev_ind]){
-            take = 1 + recurr(ind+1,ind,dp,nums);
-        }
-
-        return dp[ind][prev_ind +1] = max(not_take,take);
+        return dp[ind][prev + 1] = max(pick, npick);
     }
-    int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size(),vector<int>(nums.size()+1,-1));
-        return recurr(0,-1,dp,nums);
-        
+
+    int lengthOfLIS(vector<int> &nums)
+    {
+        vector<vector<int>> dp(nums.size(), vector<int>(nums.size() + 1, INT_MIN));
+        return recc(nums, 0, -1, dp);
     }
 };
