4c01a39587d7a847bf256786769d003e12852fea
Aditya Malu <98596211+AdityaMalu@users.noreply.github.com>
2024-07-27 23:06:10 +0530

    Time: 3 ms (92.76%), Space: 12.9 MB (30.24%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,47 +1,27 @@
 class Solution {
 public:
-    int recur(string &word1,string &word2, int ind1, int ind2,vector<vector<int>> &dp){
-        if(ind1 < 0){
+    
+    int recc(string &word1, int ind1 , string &word2 , int ind2,vector<vector<int>> &dp){
+        if(ind1 <0){
             return ind2+1;
         }
         if(ind2<0){
             return ind1+1;
         }
 
-        if(dp[ind1][ind2] != -1){
-            return dp[ind1][ind2];
-        }
-
+        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
         if(word1[ind1] == word2[ind2]){
-           return recur(word1,word2,ind1-1,ind2-1,dp);
+            return dp[ind1][ind2] =  recc(word1,ind1-1,word2,ind2-1,dp);
         }
         else{
-            return dp[ind1][ind2] = 1 + min(recur(word1,word2,ind1-1,ind2,dp),min(recur(word1,word2,ind1,ind2-1,dp),recur(word1,word2,ind1-1,ind2-1,dp)));
+            return dp[ind1][ind2] =  1 + min(recc(word1,ind1-1,word2,ind2-1,dp),min(recc(word1,ind1-1,word2,ind2,dp) ,recc(word1,ind1 , word2,ind2-1,dp)));
         }
+
+
+
     }
     int minDistance(string word1, string word2) {
-        vector<vector<int>>dp(word1.size()+1,vector<int>(word2.size()+1,0));
-
-        for(int i = 0 ; i <= word1.size() ; i++){
-            dp[i][0] = i;
-        }
-
-        for(int i = 0 ; i <= word2.size() ; i++){
-            dp[0][i] = i;
-        }
-
-        for(int i = 1 ; i<=word1.size() ; i++){
-            for(int j = 1 ; j <= word2.size() ; j++){
-                if(word1[i-1] == word2[j-1]){
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] = 1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
-                }
-            }
-        }
-
-        return dp[word1.size()][word2.size()];
-        return recur(word1,word2,word1.size()-1,word2.size()-1,dp);
+        vector<vector<int>> dp(word1.size(),vector<int>(word2.size(),-1));
+        return recc(word1,word1.size()-1, word2 , word2.size()-1,dp);
     }
 };
