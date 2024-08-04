333a1576050128bb23c3d1ee71429616e150ce40
Aditya Malu <98596211+AdityaMalu@users.noreply.github.com>
2024-08-04 23:46:33 +0530

    Time: 509 ms (5.39%), Space: 27.3 MB (31.24%) - LeetHub

---
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
+        int MOD = 1e9+7;
+        vector<int> sums;
+        int ans = 0;
+        for(int i = 0 ; i < n ; i++){
+            int temp = 0;
+            for(int j = i; j < n ; j++){
+                temp += nums[j];
+                sums.push_back(temp);
+            }
+        }
+
+
+        for(auto i : sums){
+            cout<<i<<" ";
+        }
+
+        sort(sums.begin(), sums.end());
+        for(int i = left-1 ; i <=right-1 ; i++){
+            ans += sums[i];
+            ans%=MOD;
+        }
+
+        return ans%MOD;
+    }
+};
