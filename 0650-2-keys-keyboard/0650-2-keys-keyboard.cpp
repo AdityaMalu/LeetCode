9aa6affb7a7518bea77c697978f8e93828c1e311
Aditya Malu <98596211+AdityaMalu@users.noreply.github.com>
2024-08-19 10:32:51 +0530

    Time: 46 ms (8.33%), Space: 157.2 MB (9.34%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int recc(int n, int curr, int clipboard){
+    int recc(int n, int curr, int clipboard,vector<vector<int>> &dp){
         if(curr == n){
             return 0;
         }
@@ -9,9 +9,11 @@ public:
             return INT_MAX;
         }
 
+        if(dp[curr][clipboard] != -1) return dp[curr][clipboard];
+
         int paste = INT_MAX;
         if(clipboard > 0 && curr + clipboard <= n){
-            int pasteResult = recc(n, curr + clipboard, clipboard);
+            int pasteResult = recc(n, curr + clipboard, clipboard,dp);
             if (pasteResult != INT_MAX) {
                 paste = 1 + pasteResult;
             }
@@ -19,17 +21,18 @@ public:
 
         int copy = INT_MAX;
         if(curr != clipboard){
-            int copyResult = recc(n, curr, curr);
+            int copyResult = recc(n, curr, curr,dp);
             if (copyResult != INT_MAX) {
                 copy = 1 + copyResult;
             }
         }
 
-        return min(copy, paste);
+        return dp[curr][clipboard] = min(copy, paste);
     }
 
     int minSteps(int n) {
-        if (n == 1) return 0; 
-        return recc(n, 1, 0);
+        if (n == 1) return 0;
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1)); 
+        return recc(n, 1, 0,dp);
     }
 };
