8be825dfd50371c33fc542bb75c2b0a2030e5585
Aditya Malu <98596211+AdityaMalu@users.noreply.github.com>
2024-08-25 11:30:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -6,45 +6,46 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        unordered_map<int,vector<int>> mp;
+
         vector<vector<int>> ans;
-        queue<pair<TreeNode* , int>> q;
-        q.push({root,0});
-        while(!q.empty()){
+
+        if (!root)
+            return ans;
+        unordered_map<int, vector<int>> mp;
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 0});
+        while (!q.empty()) {
             auto curr = q.front();
-            TreeNode* node= curr.first;
+            TreeNode* node = curr.first;
             int lvl = curr.second;
             q.pop();
 
-            if(!node)
-                continue;
-            
-            if(mp.find(lvl) == mp.end()){
+            if (mp.find(lvl) == mp.end()) {
                 mp[lvl] = {node->val};
-            }
-            else{
+            } else {
                 mp[lvl].push_back(node->val);
             }
 
-            if(node->left){
-                q.push({node->left,lvl+1});
+            if (node->left) {
+                q.push({node->left, lvl + 1});
             }
-            if(node->right){
-                q.push({node->right,lvl+1});
+            if (node->right) {
+                q.push({node->right, lvl + 1});
             }
         }
 
-        for(auto i:mp){
+        for (auto i : mp) {
             ans.push_back(i.second);
         }
-        
-        reverse(ans.begin(),ans.end());
+
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
