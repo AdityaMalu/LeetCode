249178bbcd361c139699d5b8396cc0ffe8bc9008
Aditya Malu <98596211+AdityaMalu@users.noreply.github.com>
2024-10-04 16:59:39 +0530

    Time: 54 ms (92.6%), Space: 56.5 MB (56.4%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
@@ -4,22 +4,17 @@ public:
         sort(skill.begin(),skill.end());
         long long sum = skill[0] + skill[skill.size()-1];
         long long i = 0, j = skill.size()-1;
+        long long ans = 0;
         vector<pair<int,int>> temp;
         while(i < j){
             if(sum != skill[i] + skill[j]){
                 return -1;
             }
             else{
-                temp.push_back({skill[i],skill[j]});
+                ans += skill[i]*skill[j];
             }
             i++;
             j--;
-        }
-        long long ans = 0;
-        for(auto i : temp){
-            int first = i.first;
-            int second = i.second;
-            ans += first*second;
         }
         return ans;
     }
